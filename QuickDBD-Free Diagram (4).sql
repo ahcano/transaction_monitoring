63c95742912e334fdb8bba24fa8c285b77da9e9f
ahcano <ana_cano20@hotmail.com>
2023-10-09 19:54:25 -0400

    Adding Relationship Diagram and SQL file

---
+++ QuickDBD-Free Diagram (4).sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/L8AQ2U
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "card_holder" (
+    -- this table holds card holder names
+    "id" int   NOT NULL,
+    "name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "credit_card" (
+    -- this table holds cardholder IDs and their credit card numbers
+    "card" varchar(20)   NOT NULL,
+    "cardholder_id" int   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+CREATE TABLE "transaction" (
+    -- this table holds the transactions made by cardholders by date, amounts and merchant ID
+    "id" int   NOT NULL,
+    "date" TIMESTAMP   NOT NULL,
+    "amount" float   NOT NULL,
+    "card" varchar(20)   NOT NULL,
+    "id_merchant" int   NOT NULL,
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant_category" (
+    -- this table holds merchant categories
+    "id" int   NOT NULL,
+    "name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant" (
+    -- this table holds merchant names and category IDs
+    "id" int   NOT NULL,
+    "name" varchar(20)   NOT NULL,
+    "id_merchant_category" int   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_cardholder_id" FOREIGN KEY("cardholder_id")
+REFERENCES "card_holder" ("id");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
+
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merchant_category" FOREIGN KEY("id_merchant_category")
+REFERENCES "merchant_category" ("id");
+
